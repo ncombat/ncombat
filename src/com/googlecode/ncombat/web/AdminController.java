1993b9dce917d897ed040376a9c9bae9e5d5dfaa
Charles Kilbride <charles@ncombat.org>
2009-10-02 04:48:35 +0000

    Added Spring Web MVC infrastructure.

---
+++ src/com/googlecode/ncombat/web/AdminController.java
@@ -0,0 +1,19 @@
+package com.googlecode.ncombat.web;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+
+public class AdminController extends MultiActionController
+{
+	public Map test(HttpServletRequest request, HttpServletResponse response) {
+		Map model = new HashMap();
+		model.put("msg1", "Hello, world!");
+		model.put("msg2", "AdminController says hello.");
+		return model;
+	}
+}
---
+++ src/com/googlecode/ncombat/web/JsonView.java
@@ -0,0 +1,53 @@
+package com.googlecode.ncombat.web;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.View;
+
+@SuppressWarnings("unchecked")
+public class JsonView implements View
+{
+	public static final String DEFAULT_CONTENT_TYPE = "application/json";
+	
+	public static final String ARRAY_KEY = "__jsonArray";
+	
+	public static final String CONTENT_TYPE_KEY = "__jsonContentType";
+	
+	public String getContentType() {
+		return null;
+	}
+
+	public void render(Map model, HttpServletRequest request, HttpServletResponse response) throws Exception
+	{
+		if (model == null) {
+			model = new HashMap();
+		}
+		
+		String contentType = DEFAULT_CONTENT_TYPE;
+		
+		if (model.containsKey(CONTENT_TYPE_KEY)) {
+			contentType = model.get(CONTENT_TYPE_KEY).toString();
+			model.remove(CONTENT_TYPE_KEY);
+		}
+		
+		Object jsonObject = null;
+		
+		if (model.containsKey(ARRAY_KEY)) {
+			jsonObject = model.get(ARRAY_KEY);
+		}
+		if (jsonObject == null) {
+			jsonObject = model;
+		}
+		
+		String json = new JsonWriter().toString(jsonObject);
+		
+		response.setContentType(contentType);
+		response.setContentLength(json.length());
+		
+		response.getWriter().print(json);
+	}
+}
