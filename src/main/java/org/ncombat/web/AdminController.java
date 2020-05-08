fe20bf827b18087f13266585c3a323e2c67936d6
Charles <charles@ncombat.org>
2020-05-08 14:29:37 -0500

    Upgraded Spring and switched to Java (as opposed to XML) configuration.

--- src/main/java/org/ncombat/web/AdminController.java
+++ src/main/java/org/ncombat/web/AdminController.java
@@ -1,22 +1,23 @@
 package org.ncombat.web;
 
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
-
-@SuppressWarnings("unchecked")
-public class AdminController extends MultiActionController
+@Controller
+public class AdminController
 {
-	public ModelAndView admin(HttpServletRequest request, HttpServletResponse response) {
+	@GetMapping("/admin.do")
+	public ModelAndView admin() {
 		return new ModelAndView("admin");
 	}
 
-	public Map adminTest(HttpServletRequest request, HttpServletResponse response) {
+	@GetMapping(path = "/adminTest.json", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Map adminTest() {
 		Map model = new HashMap();
 		model.put("msg1", "Hello, world!");
 		model.put("msg2", "AdminController says hello.");
---
+++ src/main/java/org/ncombat/web/WebConfig.java
@@ -0,0 +1,47 @@
+package org.ncombat.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.BeanNameViewResolver;
+import org.springframework.web.servlet.view.InternalResourceView;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public BeanNameViewResolver beanNameViewResolver() {
+        BeanNameViewResolver beanNameViewResolver = new BeanNameViewResolver();
+        beanNameViewResolver.setOrder(1);
+        return beanNameViewResolver;
+    }
+
+    @Bean
+    public InternalResourceViewResolver internalResourceViewResolver() {
+        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+        internalResourceViewResolver.setViewClass(InternalResourceView.class);
+        internalResourceViewResolver.setPrefix("WEB-INF/jsp/");
+        internalResourceViewResolver.setSuffix(".jsp");
+        internalResourceViewResolver.setOrder(2);
+        return internalResourceViewResolver;
+    }
+
+    @Bean
+    public JsonView jsonView() {
+        return new JsonView();
+    }
+
+    @Bean
+    public JsonInterceptor jsonInterceptor() {
+        return new JsonInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jsonInterceptor());
+    }
+}
