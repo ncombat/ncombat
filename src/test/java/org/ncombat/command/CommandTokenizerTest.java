dcff6f9ab5e4ba74242397e25556568983fc5d2c
Charles <charles@ncombat.org>
2020-05-08 20:12:31 -0500

    Fixed some JUnit problems that were causing warnings and preventing tests from running.

--- src/test/java/org/ncombat/command/CommandTokenizerTest.java
+++ src/test/java/org/ncombat/command/CommandTokenizerTest.java
@@ -3,8 +3,7 @@ package org.ncombat.command;
 import java.util.ArrayList;
 import java.util.List;
 
-import junit.framework.Assert;
-
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
