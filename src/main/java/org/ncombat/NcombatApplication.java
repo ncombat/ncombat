6d752830867ff7bc99096a83a11681e7000bce8c
Charles <charles@ncombat.org>
2020-05-08 19:36:22 -0500

    Moved framework to Spring Boot and switched templating from JSP to Thymeleaf.
    Note: JSP templating is not recommended with Spring Boot.

---
+++ src/main/java/org/ncombat/NcombatApplication.java
@@ -0,0 +1,12 @@
+package org.ncombat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcombatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NcombatApplication.class, args);
+	}
+}
